0557ef6d7516cf29c0d58919e5c78dcc4968a74e
Flems At School <354019@via.dk>
2025-09-03 18:02:26 +0200

    comm1

---
+++ Øv01_MathLib/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MathLib
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine($"Calling add with two numbers: {calculator.Add(2, 3)}");
+            Console.WriteLine($"Calling add with an integer array: {calculator.Add(new int[]{1,2,3,4,5})}");
+
+            calculator.ReadTwoIntegersAndPrintTheHighest();
+        }
+    }
+}
